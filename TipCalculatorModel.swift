8a15ecbd443fcb96a830d229b2a9fe642ebcfa05
Venura De Zoysa <vz.dz925@gmail.com>
2014-12-25 23:24:05 +1100

    Initial commit

---
+++ TipCalculatorModel.swift
@@ -0,0 +1,45 @@
+//
+//  TipCalculatorModel.swift
+//  Tip Calculator
+//
+//  Created by Venura De Zoysa on 25/12/2014.
+//  Copyright (c) 2014 Venura De Zoysa. All rights reserved.
+//
+
+import Foundation
+
+class TipCalculatorModel {
+    
+    var total: Double
+    var taxPct: Double
+    var subtotal: Double {
+        get {
+            return total / (taxPct+1)
+        }
+    }
+    
+    init(total: Double, taxPct: Double) {
+        self.total = total
+        self.taxPct = taxPct
+    }
+    
+    func calcTipWithTipPct(tipPct: Double) -> Double {
+        return subtotal * tipPct
+    }
+    
+    func returnPossibleTips() -> [Int: Double] {
+        
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        let possibleTipsExplicit:[Double] = [0.15, 0.18, 0.20]
+        
+        var retval = [Int: Double]()
+        for possibleTip in possibleTipsInferred {
+            let intPct = Int(possibleTip*100)
+
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+        
+    }
+    
+}
